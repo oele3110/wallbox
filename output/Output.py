fd443b1b1c4a2bffe9a08640550de7cd93bfc3e5
Dennis Lampert <oele3110@hotmail.com>
2025-03-25 17:08:33 +0100

    Handle Exception in ModbusReader

--- output/Output.py
+++ output/Output.py
@@ -49,23 +49,26 @@ class Output:
         self.current_time += timer_increment
         for modbus_key, modbus_config in modbus_wallbox_config.items():
             if modbus_config["update_frequency"] % timer_increment == 0:
-                modbus_result = self.modbusReader.read_modbus(modbus_config)
-                if modbus_config["display_line"] > 0:
-                    if "division" in modbus_config and modbus_config["division"] is not None:
-                        modbus_result = modbus_result / modbus_config["division"]
-                        if "division_round" in modbus_config and modbus_config["division_round"] is not None:
-                            modbus_result = round(modbus_result, modbus_config["division_round"])
-                            if modbus_config["division_round"] == 0:
-                                modbus_result = int(modbus_result)
-                    unit = modbus_config["unit"] if modbus_config["unit"] is not None else ""
-                    if modbus_key == "wallbox_status_code":
-                        modbus_result = modbus_wallbox_status_codes[modbus_result]
-                    display_string = modbus_config["display_string"] + ": " + str(modbus_result) + " " + unit
-                    display_string = fill_string_with_spaces(display_string, 20)
-                    self.lcd.lcd_display_string(display_string, modbus_config["display_line"])
-                elif modbus_config["display_line"] == 0:
-                    # update leds
-                    self.led.update_leds(modbus_result)
+                try:
+                    modbus_result = self.modbusReader.read_modbus(modbus_config)
+                    if modbus_config["display_line"] > 0:
+                        if "division" in modbus_config and modbus_config["division"] is not None:
+                            modbus_result = modbus_result / modbus_config["division"]
+                            if "division_round" in modbus_config and modbus_config["division_round"] is not None:
+                                modbus_result = round(modbus_result, modbus_config["division_round"])
+                                if modbus_config["division_round"] == 0:
+                                    modbus_result = int(modbus_result)
+                        unit = modbus_config["unit"] if modbus_config["unit"] is not None else ""
+                        if modbus_key == "wallbox_status_code":
+                            modbus_result = modbus_wallbox_status_codes[modbus_result]
+                        display_string = modbus_config["display_string"] + ": " + str(modbus_result) + " " + unit
+                        display_string = fill_string_with_spaces(display_string, 20)
+                        self.lcd.lcd_display_string(display_string, modbus_config["display_line"])
+                    elif modbus_config["display_line"] == 0:
+                        # update leds
+                        self.led.update_leds(modbus_result)
+                except Exception as e:
+                    print(f"Error while reading modbus: {e} for {modbus_key}")
 
         # reset current_time if timer_maximum is reached
         if self.current_time > timer_maximum:
